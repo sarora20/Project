66dd7954d054f0ef1281accd493fa4262ac248eb
Sujan A <sujan.arora@gmail.com>
2019-07-19 11:14:16 -0500

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,55 @@
+from app import app
+import flask
+from flask import Flask, render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/major_key.html')
+def major_key():
+    # return render_template('index.html')
+    return "major"
+
+
+@app.route('/minor_key.html')
+def minor_key():
+    return "minor"
+
+
+@app.route('/increase_volume.html')
+def increase_volume():
+    return "increasevolume"
+
+
+@app.route('/decrease_volume.html')
+def decrease_volume():
+    return "decreasevolume"
+
+
+@app.route('/increase_tempo.html')
+def increase_tempo():
+    return "increasetempo"
+
+
+@app.route('/decrease_tempo.html')
+def decrease_tempo():
+    return "decreasetempo"
+
+
+@app.route("/add_bassline.html")
+def add_baseline():
+    return "bassline"
+
+
+@app.route('/add_harmony.html')
+def add_harmony():
+    return "harmony"
+
+
+@app.route('/change_instrument.html')
+def change_instrument():
+    return "instrument"
---
+++ app/static/index.js
@@ -0,0 +1,142 @@
+function changeMajorKey() {
+    $.ajax({
+      url: "major_key.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function changeMinorKey() {
+    $.ajax({
+      url: "minor_key.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function increaseVolume() {
+    $.ajax({
+      url: "increase_volume.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function decreaseVolume() {
+    $.ajax({
+      url: "decrease_volume.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function increaseTempo() {
+    $.ajax({
+      url: "increase_tempo.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function decreaseTempo() {
+    $.ajax({
+      url: "decrease_tempo.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function addBassline() {
+    $.ajax({
+      url: "add_bassline.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function addHarmony() {
+    $.ajax({
+      url: "add_harmony.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
+
+
+function changeInstrument() {
+    $.ajax({
+      url: "change_instrument.html",
+      data: {
+
+      },
+      success: function(results){
+        console.log(results);
+
+
+      }
+    });
+    return false;
+}
